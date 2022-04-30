d146e9d27332f990ed4bbfc7c41c504385999fd8
DHerman42 <91754966+DHerman42@users.noreply.github.com>
2022-04-30 14:23:47 -0400

    coin flipping commit

---
+++ Week 4/Coin Flipping/CoinFlipping.js
@@ -0,0 +1,47 @@
+const tossCoin = () => {
+	return Math.random() > 0.5 ? "heads" : "tails";
+};
+
+const fiveHeadsSync = () => {
+	let headsCount = 0;
+	let attempts = 0;
+	while (headsCount < 5) {
+		attempts++;
+		let result = tossCoin();
+		console.log(`${result} was flipped`);
+		if (result === "heads") {
+			headsCount++;
+		} else {
+			headsCount = 0;
+		}
+	}
+	return `It took ${attempts} tries to flip five "heads"`;
+};
+
+const fiveHeads = () => {
+	return new Promise((resolve, reject) => {
+		let headsCount = 0;
+		let attempts = 0;
+		let maxAttempts = 100;
+
+		while (headsCount < 5 && attempts <= maxAttempts) {
+			attempts++;
+			let result = tossCoin();
+			console.log(`${result} was flipped`);
+			if (result === "heads") {
+				headsCount++;
+			} else {
+				headsCount = 0;
+			}
+		}
+		if (attempts <= maxAttempts) {
+			resolve(`It took ${attempts} tries to flip five "heads"`);
+		} else {
+			reject(`Attempts have exceded ${maxAttempts} flips.`);
+		}
+	});
+};
+
+fiveHeads()
+	.then((res) => console.log(res))
+	.catch((err) => console.log(err));
